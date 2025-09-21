23a4e10c5e91c04590276b7b3870c9b39841803e
WisdomChukwuemeka <146662289+WisdomChukwuemeka@users.noreply.github.com>
2025-09-21 16:33:24 +0100

    Update home.jsx

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -6,7 +6,7 @@ export const Homepage = () => {
   return (
     <div className="bg-gradient-to-br from-black via-black to-blue-950 text-white pt-0">
       {/* HERO SECTION */}
-      <section className="w-full relative flex items-center justify-center aspect-2/2 sm:h-50 md:h-90 lx:h-120">
+      <section className="w-full relative flex items-center justify-center sm:aspect-3/4 md:aspect-3/4 xl:aspect-3/4">
         <Image
           src="/stack/hero.png" // replace with your hero image
           alt="Hero Background"
